bf15b1f4e723146873516117aeddeed681411beb
EC2 Default User <ec2-user@ip-172-31-20-247.ap-northeast-1.compute.internal>
2022-09-25 06:46:56 +0000

    commit

--- database/migrations/2022_09_14_215500_create_members_table.php
+++ database/migrations/2022_09_14_215500_create_members_table.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+ 
     public function up()
     {
         Schema::create('members', function (Blueprint $table) {
@@ -21,11 +17,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+ 
     public function down()
     {
         Schema::dropIfExists('members');
